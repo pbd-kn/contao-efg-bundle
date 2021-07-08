578675b6562148f45738122000a6a38a8081db3f
fritzmg <fmg@inspiredminds.at>
2021-07-08 09:44:29 +0100

    revert changes to modules.php

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,6 +1,35 @@
 <?php
+
+declare(strict_types=1);
+
 // begin modules efg
-/**
+
+/*
+ *
+ *  Contao Open Source CMS
+ *
+ *  Copyright (c) 2005-2014 Leo Feyer
+ *
+ *  @package   Efg
+ *  @author    Thomas Kuhn <mail@th-kuhn.de>
+ *  @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *  @copyright Thomas Kuhn 2007-2014
+ *
+ *
+ *  Porting EFG to Contao 4
+ *  Based on EFG Contao 3 from Thomas Kuhn
+ *
+ *  @package   contao-efg-bundle
+ *  @author    Peter Broghammer <mail@pb-contao@gmx.de>
+ *  @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *  @copyright Peter Broghammer 2021-
+ *
+ *  Thomas Kuhn's Efg package has been completely converted to contao 4.9
+ *  extended by insert_tag  {{efg_insert::formalias::aliasvalue::column(::format)}}
+ *
+ */
+
+/*
  * Contao Open Source CMS
  *
  * Copyright (c) 2005-2014 Leo Feyer
@@ -11,42 +40,32 @@
  * @copyright Thomas Kuhn 2007-2014
  *
  * Porting EFG to Contao 4
- * Based on EFG Contao 3 from Thomas Kuhn 
+ * Based on EFG Contao 3 from Thomas Kuhn
  *
  * @package   contao-efg-bundle
  * @author    Peter Broghammer <mail@pb-contao@gmx.de>
  * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  * @copyright Peter Broghammer 2021-
  *
- * Thomas Kuhn's Efg package has been completely converted to contao 4.9 
+ * Thomas Kuhn's Efg package has been completely converted to contao 4.9
  * extended by insert_tag  {{efg_insert::formalias::aliasvalue::column(::format)}}
  */
 
-
 // This file is created when saving a form in form generator
-// last created on 2021-07-08 10:33:16
+// last created on 2021-04-29 12:13:12
 
-
-/**
+/*
  * Back end modules
  */
 $GLOBALS['TL_LANG']['MOD']['formdata'] = 'Formular-Daten';
 $GLOBALS['TL_LANG']['MOD']['efg_co4'] = 'Formular-Daten';
-$GLOBALS['TL_LANG']['MOD']['feedback'] = array('Feedback', 'Gespeicherte Daten aus Formularen.');
+$GLOBALS['TL_LANG']['MOD']['feedback'] = ['Feedback', 'Gespeicherte Daten aus Formularen.'];
 // Eintraege der Forms
-$GLOBALS['TL_LANG']['MOD']['fd_anfrage-de'] = array('Anfrage DE', 'Gespeicherte Daten aus Formular "Anfrage DE".');
-$GLOBALS['TL_LANG']['MOD']['fd_anfrage-en'] = array('Anfrage EN', 'Gespeicherte Daten aus Formular "Anfrage EN".');
-$GLOBALS['TL_LANG']['MOD']['fd_gutschein-de'] = array('Gutschein DE', 'Gespeicherte Daten aus Formular "Gutschein DE".');
-$GLOBALS['TL_LANG']['MOD']['fd_kontakt-de'] = array('Kontakt DE', 'Gespeicherte Daten aus Formular "Kontakt DE".');
-$GLOBALS['TL_LANG']['MOD']['fd_gutschein-en'] = array('Gutschein EN', 'Gespeicherte Daten aus Formular "Gutschein EN".');
-$GLOBALS['TL_LANG']['MOD']['fd_kontakt-en'] = array('Kontakt EN', 'Gespeicherte Daten aus Formular "Kontakt EN".');
-$GLOBALS['TL_LANG']['MOD']['fd_anfrage-landingpage-content-de'] = array('Anfrage - Landingpage Content DE', 'Gespeicherte Daten aus Formular "Anfrage - Landingpage Content DE".');
-$GLOBALS['TL_LANG']['MOD']['fd_anfrage-landingpage-content-en'] = array('Anfrage - Landingpage Content EN', 'Gespeicherte Daten aus Formular "Anfrage - Landingpage Content EN".');
+$GLOBALS['TL_LANG']['MOD']['fd_mytestformular-do'] = ['MyTESTFORMULAR Do', 'Gespeicherte Daten aus Formular "MyTESTFORMULAR Do".'];
 
-/**
+/*
  * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['formdatalisting'] = array('Auflistung Formular-Daten', 'Verwenden Sie dieses Modul dazu, die Daten einer beliebigen Formular-Daten-Tabelle im Frontend aufzulisten.');
+$GLOBALS['TL_LANG']['FMD']['formdatalisting'] = ['Auflistung Formular-Daten', 'Verwenden Sie dieses Modul dazu, die Daten einer beliebigen Formular-Daten-Tabelle im Frontend aufzulisten.'];
 
  // end modules efg
- 
